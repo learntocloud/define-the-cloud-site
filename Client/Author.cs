5adb29d79c8ee014ca7f4636843574ea9b40dfb5
Gwyneth Pena-Siguenza <gwynethpena@outlook.com>
2023-02-09 09:31:54 -0500

    Implmented definitions page on index

---
+++ Client/Author.cs
@@ -0,0 +1,14 @@
+
+public class Author
+{
+    public Author(string name, string link)
+    {
+        Name = name;
+        Link = link;
+    }
+
+
+    public string Name { get; set; }
+
+    public string Link { get; set; }
+}
--- Client/WordDefinition.cs
+++ Client/WordDefinition.cs
@@ -1,12 +1,21 @@
 using System.Text.Json.Serialization;
 
 
+
 public class WordDefinition
 {
 
     public string Word { get; set; }
 
-    public string Tag {get;set;}
+        public string Content { get; set; }
+
+        public Author Author { get; set; }
+
+        public string LearnMoreUrl { get; set; }
+
+        public string Tag { get; set; }
+
+        public string Abbreviation { get; set; }
 
 
 
