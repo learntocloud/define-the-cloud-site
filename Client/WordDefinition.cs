b1af548f4280d6b400b7de2921f3ab095f6752b0
Gwyneth Pena-Siguenza <gwynethpena@outlook.com>
2023-05-26 22:06:38 -0400

    Added detailed page for each definition

--- Client/WordDefinition.cs
+++ Client/WordDefinition.cs
@@ -1,21 +1,19 @@
-using System.Text.Json.Serialization;
-
-
-
 public class WordDefinition
 {
 
     public string Word { get; set; }
 
-        public string Content { get; set; }
+    public string Id { get; set; }
 
-        public Author Author { get; set; }
+    public string Content { get; set; }
 
-        public string LearnMoreUrl { get; set; }
+    public Author Author { get; set; }
 
-        public string Tag { get; set; }
+    public string LearnMoreUrl { get; set; }
 
-        public string Abbreviation { get; set; }
+    public string Tag { get; set; }
+
+    public string Abbreviation { get; set; }
 
 
 
