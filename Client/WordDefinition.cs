f97abc1d39d904d8c9227ae778c391dea8e2ec01
Gwyneth Pena-Siguenza <gwynethpena@outlook.com>
2023-05-27 11:38:01 -0400

    Updated to slug appears wit word instead of ID

--- Client/WordDefinition.cs
+++ Client/WordDefinition.cs
@@ -1,20 +1,13 @@
 public class WordDefinition
 {
-
     public string Word { get; set; }
 
     public string Id { get; set; }
 
     public string Content { get; set; }
-
     public Author Author { get; set; }
-
     public string LearnMoreUrl { get; set; }
-
     public string Tag { get; set; }
-
     public string Abbreviation { get; set; }
-
-
 
 }
