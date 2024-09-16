1b97057214d91938596528200551133bb02d5ea1
Gwyneth Pena-Siguenza <gwynethpena@outlook.com>
2024-09-16 11:56:30 -0400

    Add CloudProjectIdea feature and update dependencies
    Updated BlazorBasic.csproj to include Newtonsoft.Json 13.0.3.
    Modified Definition.razor to display cloudProjectIdea details.
    Replaced words loop with cloudProjectIdea.Steps loop.
    Added section for cloudProjectIdea.Resources link.
    Updated @code section to fetch cloudProjectIdea data.
    Removed unspecified code from Index.razor.
    Added CloudProjectIdea.cs defining CloudProjectIdea class.

---
+++ Client/CloudProjectIdea.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+
+namespace BlazorBasic
+{
+    public class CloudProjectIdea
+    {
+        
+            public string? Title { get; set; }
+            public string? Description { get; set; }
+            public List<string>? Steps { get; set; }
+
+            [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+            public List<string> Resources { get; set; } = new List<string>();
+
+
+        
+    }
+}
